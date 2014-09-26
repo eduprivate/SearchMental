9d7a8f8b4c1c6c0128b0ec6418d9cf897b6998cd
eduprivate <edu.private@gmail.com>
2014-09-25 21:01:21 -0300

    testes!

---
+++ src/test/java/br/com/sentiment/engine/SentimentEngineTest.java
@@ -0,0 +1,16 @@
+package br.com.sentiment.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SentimentEngineTest {
+	
+	
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
